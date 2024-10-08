b52e7df94b2d9e96c9c81933dc752e083f9bd279
Henrique Carvalho <136594312+HACarvalho83@users.noreply.github.com>
2024-10-08 08:37:32 -0300

    Inserido "for" exerc Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,12 +1,16 @@
 function tabuada() {
     var txtNum = document.getElementById('txtNum')
-    //var res = document.querySelector('div#res')
-    var res1 = document.getElementById('div#res')
     var resultado = ''
-    res1.innerHTML = `Oi`
-    //for(var c = 1;c <= 5;c++) {
-       //resultado = Number(txtNum.value) * c
-        //res.appendChild(`<option>${txtNum} x ${c} = ${resultado}</option>`)
-        
-    //}
+    res.innerHTML = ''
+    if (txtNum.value == '') {
+        alert('Por favor, digite um número!')
+    } else {
+        for(var c = 1;c <= 10;c++) {
+            resultado = Number(txtNum.value) * c
+             //res.appendChild(`<option>${txtNum} x ${c} = ${resultado}</option>`)
+             res.innerHTML += `<p>${txtNum.value} x ${c} = ${resultado}<p/>`
+             
+         }
+    }
+    
 }
