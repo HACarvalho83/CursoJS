09d4e7db9b530434b2bc02246fbf4a0ed46cd922
Henrique Carvalho <136594312+HACarvalho83@users.noreply.github.com>
2024-10-08 08:19:47 -0300

    Criado Exercício Tabuada

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,12 @@
+function tabuada() {
+    var txtNum = document.getElementById('txtNum')
+    //var res = document.querySelector('div#res')
+    var res1 = document.getElementById('div#res')
+    var resultado = ''
+    res1.innerHTML = `Oi`
+    //for(var c = 1;c <= 5;c++) {
+       //resultado = Number(txtNum.value) * c
+        //res.appendChild(`<option>${txtNum} x ${c} = ${resultado}</option>`)
+        
+    //}
+}
