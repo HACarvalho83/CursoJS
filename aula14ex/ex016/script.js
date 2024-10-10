d100f98942dfabb9b0d0064006460510e36db579
Henrique Carvalho <136594312+HACarvalho83@users.noreply.github.com>
2024-10-10 14:41:58 -0300

    Exercício 16 melhorado depois da correção Guanabara

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,20 +2,32 @@ function contar() {
     var nInicio = document.getElementById('txtInicio')
     var nFim = document.getElementById('txtFim')
     var nPasso = document.getElementById('txtPasso')
-    var res = document.querySelector('div#res')
-    var c = Number(nInicio.value)
-    res.innerHTML = ''   
-    if (nInicio.value == '') {
+    var res = document.querySelector('div#res')  
+    
+    if (nInicio.value == '' || nFim.value == '' || nPasso.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
     } else {
-        if (nPasso.value == 0) {
+        res.innerHTML = 'Contando:'
+        var i = Number(nInicio.value)
+        var f = Number(nFim.value)
+        var p = Number(nPasso.value)
+        var c = i
+        if (p <= 0) {
             window.alert('Passo Inválido! Considerando PASSO 1')
-            nPasso.value = 1
-        }   
-        while (c <= nFim.value) {
-            res.innerHTML += `${c}`
-            res.innerHTML += `&#128073;`
-            c += Number(nPasso.value)
+            p = 1
+        }
+        if (i < f) {  
+            // Contagem crescente 
+            while (c <= f) {
+                res.innerHTML += ` ${c} &#128073;`
+                c += p
+            }
+        } else {
+            // Contagem regreciva
+            while (c >= f) {
+                res.innerHTML += ` ${c} &#128073;`
+                c-= p 
+            }
         }
         res.innerHTML += `&#127937;`
     }    
