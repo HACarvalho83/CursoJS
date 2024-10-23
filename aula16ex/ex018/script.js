e5794cc3aed07b768b299e284659a35ed96df0c9
Henrique Carvalho <136594312+HACarvalho83@users.noreply.github.com>
2024-10-23 08:47:03 -0300

    Área de analise completa

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,18 +1,52 @@
+function jaExiste(lista1, num1) {
+    if (lista1.indexOf(num1) == -1) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function compararNumeros(a, b) {
+    return a - b;
+}
+
+function soma(lista1) {
+
+}
+
 let lista = []
-function adicionar() {
+
+function adicionar() { 
     let num = document.getElementById('inum')
     let tab = document.getElementById('imostrador')
     let item = document.createElement('option')
     let n = Number(num.value)
-    let analise = document.querySelector('div#analise')
-    if (num.value.length == 0 || num.value < 1 || num.value > 100) {
-        window.alert(`Valor inválido ou já encontrado na lista`)
+    let existe = jaExiste(lista, n)
+    if (num.value.length == 0 || num.value < 1 || num.value > 100 || existe == true) {
+        window.alert(`Valor inválido ou já encontrado na lista.`)
     } else {
         lista.push(n)
         item.text = `Valor ${n} adicionado.`
         //item.value =`tab${n}`
         tab.appendChild(item)
+        num.value = ''
+    }
+}
 
-        analise.innerHTML = `${lista}`
+function finalizar() {
+    if (lista.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        lista.sort(compararNumeros)
+
+        let analise = document.querySelector('div#iAnalise')
+        
+        analise.innerHTML = `<p>Ao todo, temos ${lista.length} números cadastrados.</p>`
+
+        analise.innerHTML += `<p>O maior valor informado foi ${lista[lista.length-1]}.</p>`
+
+        analise.innerHTML += `<p>O menor valor informado foi ${lista[0]}</p>`
+
+        analise.innerHTML += `${lista}`
     }
 }
